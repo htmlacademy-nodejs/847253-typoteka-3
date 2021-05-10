cb9d74c63b60a565dc683387404e02c971944597
Маклай Михаил Андреевич <mikhailmaclay@gmail.com>
2021-05-10 22:32:19 +0300

    Добавляет генератор тестовых данных

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+
+const {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  COMMAND_ARGV_INDEX,
+  ExitCode,
+} = require(`../constants`);
+
+const userArgs = process.argv.slice(USER_ARGV_INDEX);
+const commandArgs = process.argv.slice(COMMAND_ARGV_INDEX);
+const [command] = userArgs;
+
+if (userArgs.length === 0 || !Cli[command]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+Cli[command].run(commandArgs);
